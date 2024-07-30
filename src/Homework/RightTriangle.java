b825e248e40690e10bdc6d5b461224733339f3e2
shilparadke <shilpa.dhandare@gmail.com>
2024-07-30 15:22:39 +0530

    Please commit and push these files

---
+++ src/Homework/RightTriangle.java
@@ -0,0 +1,19 @@
+package Homework;
+
+//import java.util.Scanner;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+      //  Scanner sc=new Scanner(System.in);
+     //   int num = sc.nextInt();
+        int num=5;
+        for(int i=0; i<num;i++){
+
+            for(int j=0;j<=i;j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println(" ");
+        }
+    }
+}

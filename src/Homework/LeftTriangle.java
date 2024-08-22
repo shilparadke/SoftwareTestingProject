1254121a2b4baf555a23a223b6bd8717220b244e
shilparadke <shilpa.dhandare@gmail.com>
2024-08-22 14:42:45 +0530

    Please commit and push these files

---
+++ src/Homework/LeftTriangle.java
@@ -0,0 +1,25 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class LeftTriangle {
+
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        int num = 5;
+        // int num=5;
+        for (int i = 5; i >=0; i--) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+
+            }
+            System.out.println(" ");
+            //  sc.close();
+          /*  System.out.println(); */
+
+
+
+        }
+    }
+}
